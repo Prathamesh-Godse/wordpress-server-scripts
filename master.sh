298beab0f5b2b69b6a1d512a77da0d43de4de43b
Prathamesh Godse <121411511+Prathamesh-Godse@users.noreply.github.com>
2025-08-20 08:32:47 +0530

    Update master.sh

--- master.sh
+++ master.sh
@@ -3,3 +3,15 @@
 echo "Starting main script..."
 
 ./script/ask_details.sh
+
+
+setup_firewall.sh
+setup_fail2ban.sh
+setup_timezone.sh
+disable_swap.sh
+enable_swap.sh
+tune_sysctl.sh
+sysctl_info.sh
+harden_shm.sh
+check_shm.sh
+disable_ipv6.sh
